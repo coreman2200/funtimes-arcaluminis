6c4c1189f7438962e3fc060d2010d42de9eb2a2a
Cory H <coreman2200@gmail.com>
2025-10-16 05:37:57 -0700

    fix fake -> scenes

--- ledcube/internal/app/register_wails.go
+++ ledcube/internal/app/register_wails.go
@@ -4,8 +4,8 @@ package app
 
 import (
 	"github.com/coreman2200/funtimes-arcaluminis/ledcube/internal/render"
-	grad "github.com/coreman2200/funtimes-arcaluminis/ledcube/internal/render/fake/grad"
-	solid "github.com/coreman2200/funtimes-arcaluminis/ledcube/internal/render/fake/solid"
+	grad "github.com/coreman2200/funtimes-arcaluminis/ledcube/internal/render/scenes/grad"
+	solid "github.com/coreman2200/funtimes-arcaluminis/ledcube/internal/render/scenes/solid"
 )
 
 func registerDefaultRenderers(reg *render.Registry) {
